e9052f04945f9a823a32e76cf40090a4a8bf83d3
José I. Pérez S <jperez.sabatini@gmail.com>
2021-08-28 16:33:09 -0300

    primer commit con NavBar

---
+++ src/EcommerceApp.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Compone</h1>
+    </div>
+  );
+}
+
+export default App;
